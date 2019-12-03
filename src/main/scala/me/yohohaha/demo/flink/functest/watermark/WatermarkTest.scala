1979cf5e6a80f46269f44253dcb83c373b89dcb6
yohohaha <390342156@qq.com>
2019-12-04 00:10:14 +0800

    update watermark test

--- src/main/scala/me/yohohaha/demo/flink/functest/watermark/WatermarkTest.scala
+++ src/main/scala/me/yohohaha/demo/flink/functest/watermark/WatermarkTest.scala
@@ -1,18 +1,19 @@
 package me.yohohaha.demo.flink.functest.watermark
 
+import java.nio.charset.StandardCharsets
 import java.util.Properties
 
 import org.apache.flink.api.common.functions.AggregateFunction
-import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.common.typeinfo.{TypeHint, TypeInformation}
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer, KafkaDeserializationSchema}
 import org.apache.flink.util.Collector
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 
 /**
  * created at 2019/12/02 16:45:05
@@ -29,29 +30,51 @@ object WatermarkTest {
     properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test")
     properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
-    val watermarkConsumer = new FlinkKafkaConsumer[String]("watermark-test-3", new SimpleStringSchema, properties)
+    val watermarkConsumer = new FlinkKafkaConsumer[KafkaData]("watermark-test-3", new MyKafkaDataDeserializationSchema, properties)
     watermarkConsumer.setStartFromLatest()
 
+    env.addSource(watermarkConsumer)
+
+
     val wm = env.addSource(watermarkConsumer)
-      .map(s => {
-        val splits = s.split(",")
-        (splits(0), splits(1).toLong)
+      .map(kafkaData => {
+        val splits = kafkaData.value.split(",")
+        ExtractedData(kafkaData, splits(0), splits(1).toLong)
       })
-      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[(String, Long)](Time.milliseconds(1)) {
-        override def extractTimestamp(element: (String, Long)): Long = element._2
-      }).setParallelism(1)
-    wm.map(e => (e._1, 1L))
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[ExtractedData](Time.milliseconds(1)) {
+        override def extractTimestamp(element: ExtractedData): Long = {
+          println(s"partition=${element.kafkaData.partition} | offset=${element.kafkaData.offset} | value=${element.kafkaData.value} | emit timestamp=${element.timestamp}")
+          element.timestamp
+        }
+      })
+      .map(e => {
+        (e.value, 1L)
+      })
       .keyBy(_._1)
-      .timeWindow(Time.milliseconds(3), Time.milliseconds(1))
+      .timeWindow(Time.milliseconds(1))
       .aggregate(new CountAggreate, new ProcessWindowFunction[Long, (String, Long, String), String, TimeWindow] {
         override def process(key: String, context: Context, elements: Iterable[Long], out: Collector[(String, Long, String)]): Unit = {
           val element = elements.iterator.next
-          out.collect((key, element, s"watermark:${context.currentWatermark} window:${context.window.getStart} to ${context.window.getEnd}"))
+          out.collect((key, element, s"watermark=${context.currentWatermark} window=[${context.window.getStart}, ${context.window.getEnd - 1}]"))
         }
-      }).print("result")
+      }).print("result").setParallelism(1)
 
     env.execute("watermark test")
   }
+}
+
+case class ExtractedData(kafkaData: KafkaData, value: String, timestamp: Long)
+
+case class KafkaData(partition: Int, timestamp: Long, offset: Long, key: String, value: String)
+
+class MyKafkaDataDeserializationSchema extends KafkaDeserializationSchema[KafkaData] {
+  override def isEndOfStream(nextElement: KafkaData): Boolean = false
+
+  override def deserialize(record: ConsumerRecord[Array[Byte], Array[Byte]]): KafkaData = {
+    KafkaData(record.partition, record.timestamp, record.offset, Option(record.key).map(v => new String(v, StandardCharsets.UTF_8)).orNull, new String(record.value, StandardCharsets.UTF_8))
+  }
+
+  override def getProducedType: TypeInformation[KafkaData] = TypeInformation.of(new TypeHint[KafkaData] {})
 }
 
 class CountAggreate extends AggregateFunction[(String, Long), Long, Long] {
